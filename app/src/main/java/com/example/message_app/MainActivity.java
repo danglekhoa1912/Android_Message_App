d7be13efc120c2a65b3adda756d5658f111b0e61
danglekhoa1912 <dangkhoa123bn@gmail.com>
2022-04-07 15:44:03 +0700

    update UI

--- app/src/main/java/com/example/message_app/MainActivity.java
+++ app/src/main/java/com/example/message_app/MainActivity.java
@@ -1,49 +1,72 @@
 package com.example.message_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.message_app.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvUserName;
-    private Button btnGetApi;
-    FirebaseAuth mAuth;
-    FirebaseUser mUser;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
+    private Intent myIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
-        AnhXa();
-
         mAuth=FirebaseAuth.getInstance();
-        mUser=mAuth.getCurrentUser();
-        btnGetApi.setOnClickListener(new View.OnClickListener() {
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                Intent myIntent=new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(myIntent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id= item.getItemId();
+                if(id==R.id.btnLogOut){
+                    mAuth.signOut();
+                    myIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(myIntent);
+                    finish();
+                }
+                return false;
             }
         });
     }
 
-    private void AnhXa(){
-        tvUserName=findViewById(R.id.tvuserName);
-        btnGetApi=findViewById(R.id.btnGetApi);
-    }
 
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 
 
 }
