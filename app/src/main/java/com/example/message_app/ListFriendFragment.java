6d6f54691e97f5ce91b47e64cab0d453b713ed06
LEHOANGGLAM <lelam1384@gmail.com>
2022-05-07 23:00:40 +0700

    la

--- app/src/main/java/com/example/message_app/ListFriendFragment.java
+++ app/src/main/java/com/example/message_app/ListFriendFragment.java
@@ -7,10 +7,13 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.message_app.Adapter.UserAdapter;
 import com.example.message_app.model.User;
@@ -20,16 +23,18 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ListFriendFragment extends Fragment {
     private RecyclerView recyclerView;
     private UserAdapter userAdapter;
     private List<String> userIdList;
-
+    private EditText search_users;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,7 +47,53 @@ public class ListFriendFragment extends Fragment {
         userIdList = new ArrayList<>();
         readFriendUsers();
 
+        search_users = view.findViewById(R.id.seach_edit_text);
+        search_users.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                searchUsers(charSequence.toString().toLowerCase());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         return view;
+    }
+
+    private void searchUsers(String input){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        Query query = FirebaseDatabase.getInstance().getReference("User").orderByChild("username")
+                .startAt(input).endAt(input + "\uf8ff"); // uf8ff là tất cả ký tự Unicode
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    userIdList.clear();
+                    for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                        User user = dataSnapshot.getValue(User.class);
+
+                        userIdList.add(user.toString());
+                        userAdapter = new UserAdapter(getContext(), userIdList);
+                        recyclerView.setAdapter(userAdapter);
+                    }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void readFriendUsers(){
@@ -52,11 +103,13 @@ public class ListFriendFragment extends Fragment {
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                userIdList.clear();
-                User userCurrent = dataSnapshot.getValue(User.class);
-                userIdList=userCurrent.getListFriend();
-                userAdapter = new UserAdapter(getContext(), userIdList);
-                recyclerView.setAdapter(userAdapter);
+                if(search_users.getText().toString().equals("")) {
+                    userIdList.clear();
+                    User userCurrent = dataSnapshot.getValue(User.class);
+                    userIdList = userCurrent.getListFriend();
+                    userAdapter = new UserAdapter(getContext(), userIdList);
+                    recyclerView.setAdapter(userAdapter);
+                }
             }
 
             @Override
